7838c87ea01b26148c6839d77808d0e73ae202a8
sriguna2020 <mailtosriguna@gmail.com>
2020-05-01 12:22:19 +0530

    Second commit

--- src/test/java/com/sriguna/jenkins/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/sriguna/jenkins/springjenkins/SpringJenkinsApplicationTests.java
@@ -1,7 +1,5 @@
 package com.sriguna.jenkins.springjenkins;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,13 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
-	Logger log = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	public static Logger log = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
 
-		log.info("Test case executing...");
-		assertEquals(true, true);
 		log.info("Test case successful...");
 	}
 
